644427c037301014a0db8d5e16707b472ce18e25
Cleóbulo Oliveira <cleobulo.oliveira@itriad.org.br>
2020-06-15 15:47:50 -0400

    :sparkles:       Using the backend

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,4 +1,46 @@
 import * as React from 'react';
 import { Greetings } from './components/Greetings';
+import { todoList } from './controller';
 
-export const App = () => (<Greetings greeting="Great! App works..." ></Greetings>);
+interface StateType {
+    project: string;
+    version: string;
+}
+
+export class App extends React.Component<any, StateType> {
+
+    private _isMounted = true;
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            project: ``,
+            version: ``
+        };
+    }
+    
+    async componentDidMount() {
+        const { data } = await todoList.getConfig();
+        
+        if (this._isMounted) {
+            this.setState({
+                project: data.project,
+                version: data.version
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    render() {
+        const { project, version } = this.state;
+
+        return (
+            <Greetings greeting={`Application from ${project} project v${version} running...`}></Greetings>
+        );
+    }
+
+}
