7180b8d4e02edc6e9253a42cdc9780925226eaad
Cleóbulo Oliveira <cleobulo.oliveira@itriad.org.br>
2020-06-17 14:18:46 -0400

    feat(navbar): component that provides menu navigation

---
+++ src/frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+
+export const Navbar = () => (
+    <div className="navbar">
+        <div className="menu">
+            <ul>
+                <li><a className="button-menu" href="#">About</a></li>
+                <li><a className="button-menu" href="#">Home</a></li>
+            </ul>
+        </div>
+    </div>
+); 
