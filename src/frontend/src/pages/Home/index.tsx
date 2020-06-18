2754b567db86f46533f2d3fae99649691cd104af
Cleóbulo Oliveira <cleobulo.oliveira@itriad.org.br>
2020-06-18 16:10:07 -0400

    fix(home): treatment when list is empty

--- src/frontend/src/pages/Home/index.tsx
+++ src/frontend/src/pages/Home/index.tsx
@@ -6,27 +6,24 @@ export const Home = () => {
     let isLoading = true;
     const [textInput, setTextInput] = React.useState(`http://`);
     const [urlList, setUrlList] = React.useState([]);
+    const loadUrlList = async () => {
+        const geturlsResponse = await todoList.getUrls();
+
+        if (geturlsResponse.data) {
+            const { hits } = geturlsResponse.data.hits;
+            setUrlList(hits);
+        }
+    };
     const handleChange = (event: any) => (setTextInput(event.target.value));
     const onClickDownload = async () => {
         const doc = textInput.startsWith('http://') ? textInput : `http://${textInput}`;
         const addResponse = await todoList.addUrl(doc);
         
-        if (addResponse.data) {
-            const geturlsResponse = await todoList.getUrls();
-            const { hits } = geturlsResponse.data.hits;
-            setUrlList(hits);
-        }
+        if (addResponse.data) loadUrlList();
     };
 
     React.useEffect(() => {
-        const loadUrlList = async () => {
-            const geturlsResponse = await todoList.getUrls();
-            const { hits } = geturlsResponse.data.hits;
-            setUrlList(hits);
-        };
-
-        if (isLoading) loadUrlList();
-        isLoading = false;
+        loadUrlList();
     }, []);
     
     return (
