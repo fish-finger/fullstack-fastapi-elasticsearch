b91ad553cd40ce0040fe745062ac3c6d7eeaebd0
Cleóbulo Oliveira <cleobulo.oliveira@itriad.org.br>
2020-06-18 08:54:18 -0400

    refactor(navbar): change the navigation menu items order

--- src/frontend/src/components/Navbar/index.tsx
+++ src/frontend/src/components/Navbar/index.tsx
@@ -6,8 +6,8 @@ export const Navbar = () => (
     <div className="navbar">
         <div className="menu">
             <ul>
-                <li><Link to="/about" className="button-menu">About</Link></li>
                 <li><Link to="/" className="button-menu">Home</Link></li>
+                <li><Link to="/about" className="button-menu">About</Link></li>
             </ul>
         </div>
     </div>
